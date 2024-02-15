b17992a2ab6842ef17aa22888efd863de4c0c831
joaquimpacheco21 <87371978+pacheco2126@users.noreply.github.com>
2024-02-15 19:30:28 +0100

    Added inspector, and funds, and modifiers, requires, and lender transaction.

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -7,22 +7,75 @@ interface IERC721{
 contract Escrow {
     address public nftAddress;
     uint256 public nftID;
+    uint256 public purchasePrice;
+    uint256 public escrowAmount;
     address payable public seller; //address is address annd solidity of a user 
     address payable public buyer; //payable means that can actually receive ether funds or send
+    address public inspector;
+    address public lender;
+
+    modifier onlyBuyer() { //make a conditional cleaner and usable in other functions
+    require(msg.sender== buyer, "Only buyer can call this function"); //check that the sender is the buyer 
+    _; //it just return the function body
+    }
+       modifier onlyInspector() { //make a conditional cleaner and usable in other functions
+    require(msg.sender== inspector, "Only inspector can call this function"); //check that the sender is the buyer 
+    _; //it just return the function body
+    }
+
+    bool public inspectionPassed = false;
+    mapping (address =>  bool) public approval;
+    
+    //we can add extra logic but for the tutorial its good like this in order to allow receiving ether
+    receive() external payable {}
 
     constructor(
         address _nftAddress,
         uint256 _nftID,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount,
         address payable _seller,
-        address payable _buyer
+        address payable _buyer,
+        address _inspector,
+        address _lender
     ){
         nftAddress = _nftAddress;
         nftID = _nftID;
+        purchasePrice = _purchasePrice;
+        escrowAmount = _escrowAmount;
         seller = _seller;
         buyer = _buyer;
+        inspector = _inspector;
+        lender = _lender;
 }
 
+
+//funct that lets call the funct and send in crypto at the same time with payable
+function depositEarnest() public payable onlyBuyer {
+    require(msg.value >= escrowAmount); //msg global keyword, the value has to be >=
+}
+
+function uptdateInspectionStauts(bool _passed) public onlyInspector {
+    inspectionPassed = _passed;
+}
+
+function approveSale() public { //write the mapping w/ msg sender which is the address is calling the function
+    approval[msg.sender] = true;
+}
+
+function getBalance() public view returns (uint) {
+    return address(this).balance;
+    
+}
     function finalizeSale() public {
+        require(inspectionPassed, "Must pass the inspection");
+        require(approval[buyer], 'must be approved by buyer');
+        require(approval[seller], 'must be approved by seller');
+        require(approval[lender], 'must be approved by lender');
+        require(address(this).balance >= purchasePrice, 'must have enought ether for sale');
+        (bool success, )= payable(seller).call{value: address(this).balance}("");
+        require(success);
+
         //transfer owneership of property
         IERC721(nftAddress).transferFrom(seller, buyer, nftID);
     }
--- test/RealEstate.js
+++ test/RealEstate.js
@@ -1,17 +1,25 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
+const tokens = (n) => ethers.parseUnits(n.toString(), 'ether')
+  
+
+  const ether = tokens
+  
 describe('RealEstate', () => { 
     let realEstate, escrow
     let deployer, seller
     let nftID = 1
-    
+    let purchasePrice = ether(100)
+    let escrowAmount = ether(20)
     beforeEach(async () => {
         // Setup accounts
         accounts = await ethers.getSigners()
         deployer = accounts[0]
         seller = deployer
         buyer = accounts[1]
+        inspector = accounts[2]
+        lender = accounts[3]
 
    // Load contracts
    const RealEstate = await ethers.getContractFactory('RealEstate')
@@ -22,8 +30,12 @@ describe('RealEstate', () => {
     escrow = await Escrow.deploy(
      realEstate.getAddress(), 
      nftID,
+     purchasePrice,
+     escrowAmount,
      seller,
-     buyer
+     buyer,
+     inspector.address,
+     lender.address
     )
 
     //seller approves NFT
@@ -41,11 +53,52 @@ describe('Deployment', async () => {
 }) 
 
 describe('Selling real Estate', async () => { 
-
+let transaction, balance
     it('execute a successful transaction', async () => {
         // Excpects sellter to be nft owner before the sale
         expect(await realEstate.ownerOf(nftID)).to.equal(seller.address)
-        
+      
+        //check escrow balance before transaction
+        balance = await escrow.getBalance()
+        console.log("escrow balance:", ethers.formatEther(balance))
+
+
+
+        //buyer deposit earnest
+        transaction = await escrow.connect(buyer).depositEarnest({ value: ether(20) })
+        console.log("Buyer deposits earnest money")
+
+        //check escrow balance
+        balance = await escrow.getBalance()
+        console.log("escrow balance:", ethers.formatEther(balance))
+
+        //inspector updates status
+
+        transaction = await escrow.connect(inspector).uptdateInspectionStauts(true)
+        await transaction.wait()
+        console.log("inspector updates status")
+
+        //buyer approves the sale
+        transaction = await escrow.connect(buyer).approveSale()
+        await transaction.wait()
+        console.log('buyer approves sale')
+
+         //seller approves the sale
+        transaction = await escrow.connect(seller).approveSale()
+        await transaction.wait()
+        console.log('seller approves sale')
+       
+         //lender funds sale. 
+        transaction = await lender.sendTransaction({
+            to: escrow.getAddress(), 
+            value: ether(80)
+        })
+
+        //lender approves the sale
+        transaction = await escrow.connect(lender).approveSale()
+        await transaction.wait()
+        console.log('lender approves sale')
+
         //finalize sale
 
         transaction = await escrow.connect(buyer).finalizeSale()
